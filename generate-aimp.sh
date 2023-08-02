742cf6795b32ce450c079d850eef385b34868ac7
Marc Hadfield <marc@vital.ai>
2023-08-02 09:55:59 -0400

    sync

--- generate-aimp.sh
+++
@@ -1,8 +0,0 @@
-bin/vitalsigns generate -o ../domain-ontology/vital-aimp-0.1.0.owl
-
-bin/vitalsigns generate -o ../domain-ontology/vital-aimp-0.1.0.owl -t json -or
-
-vitalmavendeploy installdomainjar -j ../domain-groovy-jar/vital-aimp-groovy-0.1.0.jar
-
-ant -f /d/workspace_vital.ai_git/workspace_haley.ai/aimp/copy_aimp.xml
-
