49bcb359ce33246e4082877c902dabfdf2e14265
Marc Hadfield <marc@vital.ai>
2024-06-17 14:08:55 -0400

    sync

---
+++ domain-python/vital-aimp-python/com_vitalai_aimp_domain/model/AIMPIntent.pyi
@@ -0,0 +1,9 @@
+
+import datetime
+from com_vitalai_aimp_domain.model.UserCommandMessage import UserCommandMessage
+
+
+class AIMPIntent(UserCommandMessage):
+        aIMPIntent: str
+        aIMPIntentType: str
+
---
+++ domain-python/vital-aimp-python/com_vitalai_aimp_domain/model/AIMPMessageContent.pyi
@@ -0,0 +1,8 @@
+
+import datetime
+from vital_ai_vitalsigns.model.VITAL_Node import VITAL_Node
+
+
+class AIMPMessageContent(VITAL_Node):
+        text: str
+
