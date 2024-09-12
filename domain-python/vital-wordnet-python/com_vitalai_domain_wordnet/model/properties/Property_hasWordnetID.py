1e1702141bb84a65ad4435dba5fc340e0792958a
Marc Hadfield <marc@vital.ai>
2024-09-12 14:40:31 -0400

    sync

--- domain-python/vital-wordnet-python/com_vitalai_domain_wordnet/model/properties/Property_hasWordnetID.py
+++ domain-python/vital-wordnet-python/com_vitalai_domain_wordnet/model/properties/Property_hasWordnetID.py
@@ -4,3 +4,4 @@ from vital_ai_vitalsigns.model.trait.PropertyTrait import PropertyTrait
 class Property_hasWordnetID(PropertyTrait):
     namespace = "http://vital.ai/ontology/vital-wordnet#"
     local_name = "hasWordnetID"
+    multiple_values = False
